5f234a6d2ce32cc62b02872ca22ba677beab4bd5
chenjuntao <cjtlp2006@163.com>
2020-01-12 12:00:35 +0800

    ignorefile

---
+++ erp/erpsrc/src/pojo/form/Weight.java
@@ -0,0 +1,47 @@
+package pojo.form;
+
+public class Weight {
+
+
+    private String myid;
+    private String num;
+    private String pic;
+    private int isok;
+
+    public void setIsok(int isok) {
+        this.isok = isok;
+    }
+
+    public int getIsok() {
+        return isok;
+    }
+
+    public void setMyid(String myid) {
+        this.myid = myid;
+    }
+
+    public String getMyid() {
+        return myid;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
